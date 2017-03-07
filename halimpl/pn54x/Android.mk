607a98133775e1acacc902b1b35991d5a6770aac
Ruchi Kandoi <kandoiruchi@google.com>
2017-03-07 10:33:35 -0800

    Added support for PN553
    Test: compiles, nci1.0 init ok.
    Change-Id: I1195cdb0beab3f9a5e54f081c507ff49adff7538

--- halimpl/pn54x/Android.mk
+++ halimpl/pn54x/Android.mk
@@ -16,6 +16,7 @@
 PN547C2 := 1
 PN548C2 := 2
 PN551   := 3
+PN553   := 4
 NQ110 := $PN547C2
 NQ120 := $PN547C2
 NQ210 := $PN548C2
@@ -30,6 +31,9 @@ endif
 ifeq ($(PN551),3)
 D_CFLAGS += -DPN551=3
 endif
+ifeq ($(PN553),4)
+D_CFLAGS += -DPN553=4
+endif
 
 #### Select the CHIP ####
 ifeq ($(NXP_CHIP_TYPE),$(PN547C2))
@@ -38,8 +42,10 @@ else ifeq ($(NXP_CHIP_TYPE),$(PN548C2))
 D_CFLAGS += -DNFC_NXP_CHIP_TYPE=PN548C2
 else ifeq ($(NXP_CHIP_TYPE),$(PN551))
 D_CFLAGS += -DNFC_NXP_CHIP_TYPE=PN551
+else ifeq ($(NXP_CHIP_TYPE),$(PN553))
+D_CFLAGS += -DNFC_NXP_CHIP_TYPE=PN553
 else
-D_CFLAGS += -DNFC_NXP_CHIP_TYPE=PN551
+D_CFLAGS += -DNFC_NXP_CHIP_TYPE=PN553
 endif
 
 LOCAL_PATH := $(call my-dir)
