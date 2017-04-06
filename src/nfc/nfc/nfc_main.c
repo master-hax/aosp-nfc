bc9aa4b3c62c869b1500864be2c311eaf1e01f4a
Love Khanna <love.khanna@nxp.com>
2017-04-06 14:31:48 +0530

    T4T pres check is  implemented using ISO-DEP presence check nak command in NCI2.0
    when T4T tag is detected ,  T4T NAK command is sent for presence check.
    The response is sent which indicates command is sent successfully or not. When the ntf is
    received , if card is present in field the status is NFA_STATUS_Ok otherwise the status is
    NFA_STATUS_FAILED.It can be configured from libnfc-brcm file.
    
    Test: compiles.
    
    Change-Id: I58abff57a5a2514c78046f4a736246568382099f

--- src/nfc/nfc/nfc_main.c
+++ src/nfc/nfc/nfc_main.c
@@ -1251,6 +1251,24 @@ uint8_t NFC_SetTraceLevel(uint8_t new_level) {
   return (nfc_cb.trace_level);
 }
 
+/*******************************************************************************
+**
+** Function         NFC_ISODEPNakPresCheck
+**
+** Description      This function is called to send the ISO DEP nak presenc
+**                  check cmd to check that the remote end point in RF field.
+**
+**                  The response from NFCC is reported by call back.The ntf
+**                  indicates success if card is present in field or failed
+**                  if card is lost.
+**
+** Returns          tNFC_STATUS
+**
+*******************************************************************************/
+tNFC_STATUS NFC_ISODEPNakPresCheck() {
+  return nci_snd_iso_dep_nak_presence_check_cmd();
+}
+
 #if (BT_TRACE_VERBOSE == TRUE)
 /*******************************************************************************
 **
