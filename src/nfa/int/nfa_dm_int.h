5e6bbc69e271593a2026fb87f1f06a6f9e826e01
Love Khanna <love.khanna@nxp.com>
2017-04-14 18:23:55 +0530

    NCI2.0 Added extended power state for routing.
    The Power State includes 3 Sub-States (1, 2 and 3) that are applicable only
    when the Device Host is in Switched On State.
    
    Sub-States are using the following mappings:
    Switched On State: Screen is on and device is unlocked
    Switched On Sub-State1: Screen is off and device is unlocked
    Switched On Sub-State2: Screen is on and device is locked
    Switched On Sub-State3: Screen is off and device is locked
    
    Test: Compiles.
    
    Change-Id: If0aaa0da64e2780c777299edddded0e07727ddea

--- src/nfa/int/nfa_dm_int.h
+++ src/nfa/int/nfa_dm_int.h
@@ -61,6 +61,7 @@ enum {
   NFA_DM_API_REG_VSC_EVT,
   NFA_DM_API_SEND_VSC_EVT,
   NFA_DM_TIMEOUT_DISABLE_EVT,
+  NFA_DM_API_SET_POWER_SUB_STATE_EVT,
   NFA_DM_MAX_EVT
 };
 
@@ -182,6 +183,12 @@ typedef struct {
   uint8_t* p_cmd_params;
 } tNFA_DM_API_SEND_VSC;
 
+/* data type for NFA_DM_API_SET_POWER_SUB_STATE_EVT */
+typedef struct {
+  NFC_HDR hdr;
+  uint8_t screen_state;
+} tNFA_DM_API_SET_POWER_SUB_STATE;
+
 /* union of all data types */
 typedef union {
   /* GKI event buffer header */
@@ -211,6 +218,8 @@ typedef union {
   tNFA_DM_API_DEACTIVATE deactivate; /* NFA_DM_API_DEACTIVATE_EVT            */
   tNFA_DM_API_SEND_VSC send_vsc;     /* NFA_DM_API_SEND_VSC_EVT              */
   tNFA_DM_API_REG_VSC reg_vsc;       /* NFA_DM_API_REG_VSC_EVT               */
+  /* NFA_DM_API_SET_POWER_SUB_STATE_EVT */
+  tNFA_DM_API_SET_POWER_SUB_STATE set_power_state;
 } tNFA_DM_MSG;
 
 /* DM RF discovery state */
@@ -530,6 +539,7 @@ typedef struct {
 
   /* NFCC power mode */
   uint8_t nfcc_pwr_mode; /* NFA_DM_PWR_MODE_FULL or NFA_DM_PWR_MODE_OFF_SLEEP */
+  uint8_t power_state;   /* current screen/power  state */
 } tNFA_DM_CB;
 
 /* Internal function prototypes */
@@ -611,6 +621,7 @@ bool nfa_dm_act_send_vsc(tNFA_DM_MSG* p_data);
 uint16_t nfa_dm_act_get_rf_disc_duration();
 bool nfa_dm_act_disable_timeout(tNFA_DM_MSG* p_data);
 bool nfa_dm_act_nfc_cback_data(tNFA_DM_MSG* p_data);
+bool nfa_dm_set_power_sub_state(tNFA_DM_MSG* p_data);
 
 void nfa_dm_proc_nfcc_power_mode(uint8_t nfcc_power_mode);
 
