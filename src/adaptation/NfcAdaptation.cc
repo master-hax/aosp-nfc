63c082b825d04cecc436f0e0bc131c1703f9021e
Andre Eisenbach <eisenbach@google.com>
2017-12-05 12:53:59 -0800

    Remove android_logmsg and trace_api
    NCI commands are logged in memory now.
    
    Test: manual
    Change-Id: Ib2660bf9f78d60de1ac201a4d257619e281608b6

--- src/adaptation/NfcAdaptation.cc
+++ src/adaptation/NfcAdaptation.cc
@@ -24,7 +24,6 @@
 
 #include "NfcAdaptation.h"
 
-#include "android_logmsg.h"
 #include "debug_nfcsnoop.h"
 #include "nfa_api.h"
 #include "nfc_config.h"
@@ -79,7 +78,11 @@ static std::vector<uint8_t> host_whitelist;
 namespace {
 void initializeGlobalDebugEnabledFlag() {
   nfc_debug_enabled =
+<<<<<<< HEAD
       (NfcConfig::getUnsigned(NAME_NFC_DEBUG_ENABLED, 0) != 0) ? true : false;
+=======
+      (NfcConfig::getUnsigned(NAME_APPL_TRACE_LEVEL, 0) != 0) ? true : false;
+>>>>>>> 8c59204... Remove android_logmsg and trace_api
 
   char valueStr[PROPERTY_VALUE_MAX] = {0};
   int len = property_get("nfc.debug_enabled", valueStr, "");
