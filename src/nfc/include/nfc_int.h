4d4d58eb4d5f9d1bc8412a9611d88c42e8ea5fb5
Love Khanna <love.khanna@nxp.com>
2018-04-10 21:10:02 +0530

    NCI2.0 Support for System Code Based Routing registration.
    Test: Perform Felica CE with System code FE0F;
    Test: NCI ACTION NTF for SCBR expected with route to eSE.
    Change-Id: I45b6646e0c7a5324cd540419e06a5d87b0d6c6fc

--- src/nfc/include/nfc_int.h
+++ src/nfc/include/nfc_int.h
@@ -214,6 +214,8 @@ typedef struct {
 
   uint8_t nci_version; /* NCI version used for NCI communication*/
 
+  bool isScbrSupported; /* CHECK SCBR SUPPORT*/
+
   uint8_t hci_packet_size; /* maximum hci payload size*/
 
   uint8_t hci_conn_credits; /* maximum conn credits for static HCI*/
