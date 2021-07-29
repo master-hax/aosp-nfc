d324064e264c03e173d18e303bb3468d6f0ec7b0
Jack Yu <jackcwyu@google.com>
2021-07-29 21:21:15 +0800

    Fail to read NDEF message on some tags
    Do not consider the write access condition byte 0xFF as invalid value.
    
    Bug: 194415715
    Test: build pass
    Change-Id: If7c916f57b2d7d3645707eba7c8f6424c5e803f9

--- src/nfc/tags/rw_t4t.cc
+++ src/nfc/tags/rw_t4t.cc
@@ -1269,8 +1269,7 @@ static bool rw_t4t_validate_cc_file(void) {
 
   if (((p_t4t->cc_file.ndef_fc.write_access > T4T_FC_WRITE_ACCESS) &&
        (p_t4t->cc_file.ndef_fc.write_access <
-        T4T_FC_WRITE_ACCESS_PROP_START)) ||
-      (p_t4t->cc_file.ndef_fc.write_access == T4T_FC_NO_WRITE_ACCESS)) {
+        T4T_FC_WRITE_ACCESS_PROP_START))) {
     LOG(ERROR) << StringPrintf("%s - Write Access (0x%02X) is invalid",
                                __func__, p_t4t->cc_file.ndef_fc.write_access);
     return false;
