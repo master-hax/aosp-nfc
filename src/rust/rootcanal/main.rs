7c49b265b0400b39ea1b5bdad368e749a5cff6a2
Alisher Alikhodjaev <alisher@google.com>
2021-09-17 13:52:44 -0700

    Add trivial rootcanal implementation
    Bug: 197333653
    Test: build ok
    Change-Id: I90e0addea6b39976016fc0aaf5d5342f03ac2a87

---
+++ src/rust/rootcanal/main.rs
@@ -0,0 +1,124 @@
+//! This connects to "rootcanal" and provides a simulated
+//! Nfc chip as well as a simulated environment.
+
+use bytes::{BufMut, BytesMut};
+use nfc_packets::nci;
+use nfc_packets::nci::CommandChild::{InitCommand, ResetCommand};
+use nfc_packets::nci::PacketBoundaryFlag;
+use nfc_packets::nci::{
+    CommandPacket, Packet, ResetResponseBuilder, ResponseBuilder, ResponsePacket,
+};
+use num_derive::{FromPrimitive, ToPrimitive};
+use std::convert::TryInto;
+use std::io::ErrorKind;
+use thiserror::Error;
+use tokio::io;
+use tokio::io::{AsyncReadExt, AsyncWriteExt, BufReader};
+use tokio::net::TcpListener;
+
+/// Result type
+type Result<T> = std::result::Result<T, RootcanalError>;
+
+#[derive(Debug, Error)]
+enum RootcanalError {
+    #[error("Socket error")]
+    IoError(#[from] io::Error),
+    #[error("Unsupported command packet")]
+    UnsupportedCommand,
+    #[error("Packet did not parse correctly")]
+    InvalidPacket,
+    #[error("Packet type not supported")]
+    UnsupportedPacket,
+}
+
+#[derive(FromPrimitive, ToPrimitive)]
+enum NciPacketType {
+    Data = 0x00,
+    Command = 0x01,
+    Response = 0x02,
+    Notification = 0x03,
+}
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:54323").await?;
+
+    let (mut sock, _) = listener.accept().await?;
+
+    tokio::spawn(async move {
+        let (mut rd, mut wr) = sock.split();
+        loop {
+            if let Err(e) = process(&mut rd, &mut wr).await {
+                match e {
+                    RootcanalError::IoError(e) if e.kind() == ErrorKind::UnexpectedEof => break,
+                    _ => panic!("Communication error: {:?}", e),
+                }
+            }
+        }
+    });
+    Ok(())
+}
+
+async fn process<R, W>(reader: &mut R, writer: &mut W) -> Result<()>
+where
+    R: AsyncReadExt + Unpin,
+    W: AsyncWriteExt + Unpin,
+{
+    let mut reader = BufReader::new(reader);
+    let mut buffer = BytesMut::with_capacity(1024);
+    let pkt_type = reader.read_u8().await?;
+    let len: usize = reader.read_u16().await?.into();
+    println!("packet {} received len={}", pkt_type, len);
+    buffer.resize(len, 0);
+    reader.read_exact(&mut buffer).await?;
+    let frozen = buffer.freeze();
+    println!("{:?}", frozen);
+    if pkt_type == NciPacketType::Command as u8 {
+        match CommandPacket::parse(&frozen) {
+            Ok(p) => command_response(writer, p).await,
+            Err(_) => Err(RootcanalError::InvalidPacket),
+        }
+    } else {
+        Err(RootcanalError::UnsupportedPacket)
+    }
+}
+
+async fn command_response<W>(out: &mut W, cmd: CommandPacket) -> Result<()>
+where
+    W: AsyncWriteExt + Unpin,
+{
+    let packet_boundary_flag = PacketBoundaryFlag::CompleteOrFinal;
+    match cmd.specialize() {
+        ResetCommand(_) => {
+            write_cmd(
+                out,
+                (ResetResponseBuilder { packet_boundary_flag, status: nci::Status::Ok })
+                    .build()
+                    .into(),
+            )
+            .await
+        }
+        InitCommand(_) => {
+            write_cmd(
+                out,
+                (ResponseBuilder { packet_boundary_flag, op: cmd.get_op(), payload: None }).build(),
+            )
+            .await
+        }
+        _ => Err(RootcanalError::UnsupportedCommand),
+    }
+}
+
+async fn write_cmd<W>(writer: &mut W, cmd: ResponsePacket) -> Result<()>
+where
+    W: AsyncWriteExt + Unpin,
+{
+    let b = cmd.to_bytes();
+    let mut data = BytesMut::with_capacity(b.len() + 3);
+    data.put_u8(NciPacketType::Response as u8);
+    data.put_u16(b.len().try_into().unwrap());
+    data.extend(b);
+    writer.write_all(&data[..]).await?;
+    println!("command written");
+    Ok(())
+}
