2c325094c9f53ff2c85be71f60cdb61832ef0786
Alisher Alikhodjaev <alisher@google.com>
2021-11-15 17:04:50 -0800

    Data and Nci channels are split
    - Nci packets are further split to Command, Response, Notification
    
    Bug: 197333653
    Test: build ok
    Change-Id: Ib7f7fe729987d4e6a91006aa4689b33d462166a5

--- src/rust/nci/flow.rs
+++ src/rust/nci/flow.rs
@@ -2,77 +2,69 @@
 //! The module ensures flow control rules and timing imposed by NCIForum standard
 
 use log::error;
-use nfc_packets::nci::{NciMsgType, NciPacket};
-use std::sync::Arc;
+use nfc_hal::Hal;
+use nfc_packets::nci::{DataPacket, NciMsgType, NciPacket};
 use tokio::select;
-use tokio::sync::mpsc::{
-    channel, unbounded_channel, Receiver, Sender, UnboundedReceiver, UnboundedSender,
-};
-use tokio::sync::Notify;
-use tokio::time::{timeout, Duration};
+use tokio::sync::mpsc::{channel, Receiver, Sender};
+use tokio::time::{sleep, Duration, Instant};
 
 /// Result type
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
 
 /// Initialize the module and connect the channels
-pub async fn init() -> (Sender<NciPacket>, Receiver<NciPacket>) {
-    let (out_tx, in_rx) = nfc_hal::init().await;
-    let cmd_out_tx = out_tx.clone();
-    let (cmd_tx, cmd_rx) = unbounded_channel();
-    // Channel to handle downstream messages
-    let (out_ext, out_int) = channel::<NciPacket>(10);
-    // Channel to handle upstream messages
-    let (in_int, in_ext) = channel::<NciPacket>(10);
-    let cmd_resp = Arc::new(Notify::new());
+pub async fn init(
+) -> (Sender<NciPacket>, Receiver<NciPacket>, Sender<DataPacket>, Receiver<DataPacket>) {
+    let hc = nfc_hal::init().await;
+    // Channel to handle nci downstream messages
+    let (out_cmd_ext, out_cmd_int) = channel::<NciPacket>(10);
+    // Channel to handle data downstream messages
+    let (out_dta_ext, out_dta_int) = channel::<DataPacket>(10);
+    // Channel to handle nci upstream messages
+    let (in_cmd_int, in_cmd_ext) = channel::<NciPacket>(10);
+    // Channel to handle data upstream messages
+    let (in_dta_int, in_dta_ext) = channel::<DataPacket>(10);
 
-    tokio::spawn(command_stream(cmd_out_tx, cmd_rx, cmd_resp.clone()));
-    tokio::spawn(main_stream(out_int, out_tx, in_rx, in_int, cmd_tx, cmd_resp));
-    (out_ext, in_ext)
+    let ic = InternalChannels { out_cmd_int, in_cmd_int, out_dta_int, in_dta_int };
+    tokio::spawn(dispatch(hc, ic));
+    (out_cmd_ext, in_cmd_ext, out_dta_ext, in_dta_ext)
 }
 
-async fn main_stream(
-    mut out_int: Receiver<NciPacket>,
-    out_tx: UnboundedSender<NciPacket>,
-    mut in_rx: UnboundedReceiver<NciPacket>,
-    in_int: Sender<NciPacket>,
-    cmd_tx: UnboundedSender<NciPacket>,
-    cmd_resp: Arc<Notify>,
-) -> Result<()> {
+struct InternalChannels {
+    out_cmd_int: Receiver<NciPacket>,
+    in_cmd_int: Sender<NciPacket>,
+    out_dta_int: Receiver<DataPacket>,
+    in_dta_int: Sender<DataPacket>,
+}
+
+async fn dispatch(mut hc: Hal, mut ic: InternalChannels) -> Result<()> {
+    let mut has_pending_cmd = false;
+    let time_out = sleep(Duration::MAX);
+    let max_deadline = time_out.deadline();
+    tokio::pin!(time_out);
     loop {
         select! {
-            Some(cmd) = in_rx.recv() => {
+            Some(cmd) = hc.in_cmd_rx.recv() => {
                 if cmd.get_mt() == NciMsgType::Response {
-                    cmd_resp.notify_one();
+                    has_pending_cmd = false;
+                    time_out.as_mut().reset(max_deadline);
                 }
-                in_int.send(cmd).await?;
+                ic.in_cmd_int.send(cmd).await?;
             }
-            Some(cmd) = out_int.recv() => {
+            Some(dta) = hc.in_dta_rx.recv() => ic.in_dta_int.send(dta).await?,
+            Some(cmd) = ic.out_cmd_int.recv(), if !has_pending_cmd  => {
                 if cmd.get_mt() == NciMsgType::Command {
-                    cmd_tx.send(cmd)?;
-                } else {
-                    out_tx.send(cmd)?;
+                    has_pending_cmd = true;
+                    time_out.as_mut().reset(Instant::now() + Duration::from_millis(20));
                 }
+                hc.out_cmd_tx.send(cmd)?;
+            }
+            Some(dta) = ic.out_dta_int.recv() => hc.out_dta_tx.send(dta)?,
+            () = &mut time_out => {
+                error!("Command processing timeout");
+                time_out.as_mut().reset(max_deadline);
+                has_pending_cmd = false;
             }
             else => break,
-        }
-    }
-    Ok(())
-}
-
-async fn command_stream(
-    out_tx: UnboundedSender<NciPacket>,
-    mut cmd_rx: UnboundedReceiver<NciPacket>,
-    cmd_resp: Arc<Notify>,
-) -> Result<()> {
-    let to = Duration::from_millis(2 * 10);
-    loop {
-        if let Some(cmd) = cmd_rx.recv().await {
-            out_tx.send(cmd)?;
-        } else {
-            break;
-        }
-        if let Err(e) = timeout(to, cmd_resp.notified()).await {
-            error!("Command flow interrupted {:?}", e);
         }
     }
     Ok(())
--- src/rust/test/main.rs
+++ src/rust/test/main.rs
@@ -4,9 +4,9 @@
 
 use log::{debug, Level};
 use logger::{self, Config};
-use nfc_packets::nci::NciPacket;
 use nfc_packets::nci::{FeatureEnable, PacketBoundaryFlag, ResetType};
 use nfc_packets::nci::{InitCommandBuilder, ResetCommandBuilder};
+use nfc_packets::nci::{NciChild, NciPacket};
 use tokio::select;
 use tokio::sync::mpsc::{Receiver, Sender};
 
@@ -16,7 +16,7 @@ type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>
 #[tokio::main]
 async fn main() -> Result<()> {
     logger::init(Config::default().with_tag_on_device("lnfc").with_min_level(Level::Trace));
-    let (out_tx, in_rx) = nfc_rnci::init().await;
+    let (out_tx, in_rx, _out_dta, _in_dta) = nfc_rnci::init().await;
     let task = tokio::spawn(command_response(in_rx));
     send_reset(&out_tx).await?;
     send_init(&out_tx).await?;
@@ -27,7 +27,15 @@ async fn main() -> Result<()> {
 async fn command_response(mut in_rx: Receiver<NciPacket>) {
     loop {
         select! {
-            Some(cmd) = in_rx.recv() => debug!("{} - response received", cmd.get_op()),
+            Some(cmd) = in_rx.recv() => {
+                match cmd.specialize() {
+                    NciChild::Response(rsp) =>
+                        debug!("{} - response received", rsp.get_cmd_op()),
+                    NciChild::Notification(ntfy) =>
+                        debug!("{} - notification received", ntfy.get_cmd_op()),
+                    _ => debug!("Unexpected packet"),
+                }
+            }
             else => break,
         }
     }
