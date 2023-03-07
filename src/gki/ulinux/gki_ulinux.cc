352b4a251c5a3996a71b9402c54f72f00f3c2224
Jack Yu <jackcwyu@google.com>
2023-03-07 17:43:15 +0800

    Fix NFC crash when running GKI_timer_update
    1.Set p_run_cond to TIMER_TICK_EXIT_COND during GKI_Shutdown()
    to make sure GKI_run() exits.
    2.Reset OsWaitEvt[BTU_TASK] while GKI_run() exits, to indicate
    GKI_shutdown() that BTU_TASK has been exited.
    
    Bug: 242952113
    Test: Nfc on/off
    Change-Id: I31811e3a1abb635f63dc7cc58aabc9718f45799e

--- src/gki/ulinux/gki_ulinux.cc
+++ src/gki/ulinux/gki_ulinux.cc
@@ -294,6 +294,10 @@ void GKI_shutdown(void) {
             TASK_MBOX_3_EVT_MASK);
       GKI_send_event(task_id - 1, EVENT_MASK(GKI_SHUTDOWN_EVT));
 
+      if (((task_id - 1) == BTU_TASK)) {
+        gki_cb.com.system_tick_running = false;
+        *p_run_cond = GKI_TIMER_TICK_EXIT_COND; /* stop system tick */
+      }
 #if (FALSE == GKI_PTHREAD_JOINABLE)
       i = 0;
 
@@ -490,6 +494,7 @@ void GKI_run(__attribute__((unused)) void* p_task_id) {
 #endif
   } /* for */
 #endif
+  gki_cb.com.OSWaitEvt[BTU_TASK] = 0;
   DLOG_IF(INFO, nfc_debug_enabled) << StringPrintf("%s exit", __func__);
 }
 
